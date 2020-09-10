7c4559ce51bd8fda1222818affe701501021b57b
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-09-10 20:58:02 +0900

    done WKWebView

---
+++ WKWebView/WKWebView/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  WKWebView
+//
+//  Created by tomoya tanaka on 2020/09/10.
+//  Copyright © 2020 Tomoya Tanaka. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView = WKWebView()
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.frame = CGRect(x: 0, y: toolBar.frame.size.height, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height * 2)
+        view.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://www.yahoo.co.jp")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("読み髪開始")
+        indicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("ロード完了")
+        
+        indicator.stopAnimating()
+    }
+
+    @IBAction func go(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+}
+
